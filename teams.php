09737cbe7e087a52de77de8ab006a3a3ce7b7ac2
Yves Shema <admin@rysapp.com>
2020-01-23 18:01:17 -0800

    Prune teams.php

--- teams.php
+++ teams.php
@@ -1,7 +1,5 @@
 <?php
 $levels = array(8,5,6,9,3,8,2,4,6,10,8,5,6,1,7,10,5,3,7,6);
-echo sizeof($levels);
-echo "<br />";
 
 sort($levels);
 
@@ -40,19 +38,4 @@ for ($i = 0; $i < 20; $i++) {
   }
 }
 
-echo "<br />";
-foreach($teamA as $playera){
-  echo $playera." ";
-}
-echo " "."=".add($teamA);
-
-echo "<br />";
-foreach($teamB as $playerb) {
-  echo $playerb." ";
-}
-echo " "."=".add($teamB);
-
-
-echo "<br />";
-echo phpinfo();
 ?>
